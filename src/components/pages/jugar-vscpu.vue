d47c9596a6e26ed9d437fe1181fbe38577712bbb
MontyBand <montypr22@gmail.com>
2018-06-09 17:50:11 +0200

    Empezando interfaz jugar vs CPU

--- src/components/pages/jugar-vscpu.vue
+++ src/components/pages/jugar-vscpu.vue
@@ -7,18 +7,83 @@
                 </button>
             </div>
             <div class="jugar_estructura">
-                <div class="item1 cartas-rival"></div>
-                <div class="item2 chinas-rival"></div>
-                <div class="item3 perfil-rival"></div>
-                <div class="item4 marcador"></div>
-                <div class="item5 mazo-rival"></div>
-                <div class="item6 chinas"></div>
-                <div class="item7 mazo-usuario"></div>
-                <div class="item8 perfil-usuario"></div>
-                <div class="item9 chinas-usuario"></div>
-                <div class="item10 cartas-usuario"></div>
-                <div class="item11 truco-usuario"></div>
-                <div class="item12 envido-usuario"></div>
+                <div class="cartas-rival">
+                    <img class="carta-izq" src="/static/img/pantalla-juego/carta-rival-izquierda.svg" alt="">
+                    <img class="carta-centro" src="/static/img/pantalla-juego/carta-rival-centro.svg" alt="">
+                    <img class="carta-drcha" src="/static/img/pantalla-juego/carta-rival-derecha.svg" alt="">
+                </div>
+                <div class="chinas-usuario chinas-rival">
+                    <div class="chinas_cuenta">
+                        <div class="imagen_bocadillo">
+                            <img src="/static/img/pantalla-juego/bocadillo.svg" alt="bocadillo">
+                            <p>4</p>
+                        </div>
+                    </div>
+                    <div class="chinas_icono">
+                        <img src="/static/img/pantalla-juego/chinas-usuario.svg" alt="chinas del usuario">
+                    </div>
+                </div>
+                <div class="perfil">
+                    <button class="btn-clasico">
+                        <img src="/static/img/pantalla-juego/logo-usuario.svg" alt="Logo de usuario">
+                    </button>
+                    <p>montyband</p>
+                </div>
+                    <div class="marcador">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">MNT</th>
+                                    <th scope="col">SVL</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td><img src="/static/img/pantalla-juego/cruz.svg" alt="cruz equivalente a dos zancas"></td>
+                                    <td><img src="/static/img/pantalla-juego/media-cruz.svg" alt="media cruz equivalente a una zanca"></td>
+                                </tr>
+                                <tr>
+                                   <td></td>
+                                   <td><img src="/static/img/pantalla-juego/media-cruz.svg" alt="media cruz equivalente a una zanca"></td>
+                                </tr>
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                <div class="mazo-rival">
+                    <button class="btn-clasico btn-mazo">
+                        <img src="/static/img/pantalla-juego/mazo-cartas.svg" alt="Monton de cartas sobrantes">
+                    </button>
+                    <p>ir al mazo</p>
+                </div>
+                <div class="chinas">
+                    <img src="/static/img/pantalla-juego/chinas.svg" alt="total de chinas">
+                </div>
+                <div class="mazo-usuario"></div>
+                <div class="vacio"></div>
+                <div class="perfil">
+                    <button class="btn-clasico">
+                        <img src="/static/img/pantalla-juego/logo-usuario.svg" alt="Logo de usuario">
+                    </button>
+                    <p>montyband</p>
+                </div>
+                <div class="chinas-usuario">
+                    <div class="chinas_cuenta">
+                        <div class="imagen_bocadillo">
+                            <img src="/static/img/pantalla-juego/bocadillo.svg" alt="bocadillo">
+                            <p>15</p>
+                        </div>
+                    </div>
+                    <div class="chinas_icono">
+                        <img src="/static/img/pantalla-juego/chinas-usuario.svg" alt="chinas del usuario">
+                    </div>
+                </div>
+                <div class="cartas-usuario"></div>
+                <div class="truco-usuario"></div>
+                <div class="envido-usuario"></div>
             </div>
         </div>
     </section>
